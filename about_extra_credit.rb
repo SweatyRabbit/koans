98953bfb60608b443978bbe69029194e2505ccd8
Ivan Hulko <ivanhulko14@gmail.com>
2021-10-05 15:13:08 +0300

    initial commit

---
+++ about_extra_credit.rb
@@ -0,0 +1,9 @@
+# EXTRA CREDIT:
+require './about_dice_project.rb'
+require './about_scoring_project.rb'
+# Create a program that will play the Greed Game.
+# Rules for the game are in GREED_RULES.TXT.
+#
+# You already have a DiceSet class and score function you can use.
+# Write a player class and a Game class to complete the project.  This
+# is a free form assignment, so approach it however you desire.
